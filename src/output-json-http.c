797d57b9c0556f4afd2a9eab511628ca243aeab1
Eric Leblond <el@stamus-networks.com>
2024-05-20 15:08:23 +0200

    output/http: remove empty line

--- src/output-json-http.c
+++ src/output-json-http.c
@@ -569,7 +569,6 @@ bool EveHttpAddMetadata(const Flow *f, uint64_t tx_id, JsonBuilder *js)
 
 void EveHttpLogAllJSONHeaders(JsonBuilder *js, Flow *f, uint64_t tx_id)
 {
-
     HtpState *htp_state = (HtpState *)FlowGetAppState(f);
     if (htp_state) {
         htp_tx_t *tx = AppLayerParserGetTx(IPPROTO_TCP, ALPROTO_HTTP1, htp_state, tx_id);
