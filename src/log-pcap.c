c012cf1cad07f0e0eb1acbbbcb8739dc888fd3b7
Eric Leblond <el@stamus-networks.com>
2025-03-02 09:07:41 +0100

    log/pcap: conditional logs pseudo pkt
    Pseudo pkt can have stream segments attached so we need to pcap
    log them in conditional mode.

--- src/log-pcap.c
+++ src/log-pcap.c
@@ -231,10 +231,6 @@ static int PcapLogCondition(ThreadVars *tv, void *thread_data, const Packet *p)
 {
     PcapLogThreadData *ptd = (PcapLogThreadData *)thread_data;
 
-    if (PKT_IS_PSEUDOPKT(p)) {
-        return FALSE;
-    }
-
     /* Log alerted flow or tagged flow */
     switch (ptd->pcap_log->conditional) {
         case LOGMODE_COND_ALL:
@@ -253,6 +249,10 @@ static int PcapLogCondition(ThreadVars *tv, void *thread_data, const Packet *p)
                 return FALSE;
             }
             break;
+    }
+
+    if (PKT_IS_PSEUDOPKT(p)) {
+        return FALSE;
     }
 
     if (IS_TUNNEL_PKT(p) && !IS_TUNNEL_ROOT_PKT(p)) {
